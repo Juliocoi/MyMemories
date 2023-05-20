cf8503f6ed3f73c5bea3db27e8a0c194691aadd2
JulioCoi <amorim.jc@gmail.com>
2023-05-20 02:30:56 -0300

    instalação do pacote CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memoriesRoutes'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
